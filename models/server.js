515c273047b479f0c3e30e35e2e210edebac8b69
luigitemu <luartemu@gmail.com>
2021-02-10 12:35:57 -0600

    primer commit

---
+++ models/server.js
@@ -0,0 +1,47 @@
+// Servidor de express
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const Sockets = require('./sockets');
+
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        // http server  
+        this.server = http.createServer(this.app);
+
+        // Configuracions de sockets
+        this.io = socketio(this.server, {  /* Configuraciones  */ });
+    }
+    middlewares() {
+        // configurar carpeta publica 
+        this.app.use(express.static(path.resolve(__dirname, '../public')));
+
+    }
+
+    configurarSockets() {
+        // 
+        new Sockets(this.io);
+    }
+
+    execute() {
+        // inicializar middlewares
+        this.middlewares();
+
+        // inicializar sockets
+        this.configurarSockets();
+
+        // Inicializar Server   
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto: ', this.port);
+        });
+    }
+
+}
+
+module.exports = Server;
