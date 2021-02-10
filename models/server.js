993d600d686e8ade91c2e00b787807ac307bad40
luigitemu <luartemu@gmail.com>
2021-02-10 12:58:57 -0600

    changes

--- models/server.js
+++ models/server.js
@@ -1,47 +1,56 @@
-// Servidor de express
+// Servidor de Express
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
-const Sockets = require('./sockets');
+const cors = require('cors');
 
+const Sockets = require('./sockets');
 
 class Server {
 
     constructor() {
+
         this.app = express();
         this.port = process.env.PORT;
 
-        // http server  
+        // Http server
         this.server = http.createServer(this.app);
 
-        // Configuracions de sockets
-        this.io = socketio(this.server, {  /* Configuraciones  */ });
+        // Configuraciones de sockets
+        this.io = socketio(this.server, { /* configuraciones */ });
     }
+
     middlewares() {
-        // configurar carpeta publica 
+        // Desplegar el directorio público
         this.app.use(express.static(path.resolve(__dirname, '../public')));
 
+        // CORS
+        this.app.use(cors());
+
     }
 
+    // Esta configuración se puede tener aquí o como propieda de clase
+    // depende mucho de lo que necesites
     configurarSockets() {
-        // 
         new Sockets(this.io);
     }
 
     execute() {
-        // inicializar middlewares
+
+        // Inicializar Middlewares
         this.middlewares();
 
-        // inicializar sockets
+        // Inicializar sockets
         this.configurarSockets();
 
-        // Inicializar Server   
+        // Inicializar Server
         this.server.listen(this.port, () => {
-            console.log('Server corriendo en puerto: ', this.port);
+            console.log('Server corriendo en puerto:', this.port);
         });
     }
 
 }
+
 
 module.exports = Server;
